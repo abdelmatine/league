41ae8f56416c58dd7680b8be89630f5f7c9e7591
abdelmatine <abdelmatinesfar@gmail.com>
2024-12-03 20:58:46 +0100

    f

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -13,6 +13,16 @@ export const projects = [
     },
     {
       id: 2,
+      name: "ELECTROCUTE",
+      role: "There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable.",
+      avatar: "/assets/electrocute.png",
+      rating: 4.5,
+      frameworks: [
+        { asset: "/icons/react.svg", url: "https://react.org/" },
+      ],
+    },
+    {
+      id: 2,
       name: "PlanIfy",
       role: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
       avatar: "/assets/cal.png",
